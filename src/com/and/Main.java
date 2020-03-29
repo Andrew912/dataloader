35586dc6f2db157c4aa4e9b03961452dbbdd5171
Andrew912 <a.obikhodov@list.ru>
2020-03-29 19:17:54 +0300

    2nd

--- src/com/and/Main.java
+++ src/com/and/Main.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 public class Main {
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        String DB_Server = "win-f4urcheiq8i";
-        String USER = "postgres";
-        String PASS = "9210603060";
+        String DB_Server = "win-f4urcheiq8i";           // Сервер БД
+        String USER = "postgres";                       // User (postgres или cuba)
+        String PASS = "9210603060";                     // пароль
         String DB_Name = "hh01";                        // имя БД
         String DB_Num = "4";                            // версия набора данных
 
